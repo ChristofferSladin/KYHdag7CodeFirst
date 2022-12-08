5f45997dd8cc9ce84f418bd813800485c9d0ac23
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2022-12-08 09:53:05 +0100

    connectionstring added

---
+++ KYHdag7CodeFirst/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KYHdag7CodeFirst.Data
+{
+    public class ApplicationDbContext
+    {
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Person> Invoice { get; set; }
+        
+        public ApplicationDbContext()
+        {
+
+        }
+    }
+}
--- KYHdag7CodeFirst/Program.cs
+++ KYHdag7CodeFirst/Program.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using KYHdag7CodeFirst.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
 var config = builder.Build();
+
+var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+var connectionString = config.GetConnectionString("DefaultConnection");
+options.UseSqlServer(connectionString);
